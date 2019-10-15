a52acef15d0ecb54cbe5f566505c2e8d4793891a
Juan Esteban Rios Arbelaez <juarios@bancolombia.com.co>
2019-10-15 16:46:48 -0500

    Se añade angular flex layout

--- frontent/src/app/app.module.ts
+++ frontent/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +29,7 @@ import { FloatButtonComponent } from './components/shared/float-button/float-but
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
